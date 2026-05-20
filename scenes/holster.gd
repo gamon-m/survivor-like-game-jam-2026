8c79b3b479d66266772d80beb21e5bc34ea898ea
gamon <kyromax.com@gmail.com>
2026-05-20 20:33:16 +0100

    add crit_chance implementation

--- scenes/holster.gd
+++ scenes/holster.gd
@@ -29,5 +29,6 @@ func shoot(target):
 
 	projectile.look_at(target.global_position)
 	projectile.damage = get_parent().damage
+	projectile.crit_chance = get_parent().crit_chance
 
 	get_tree().current_scene.add_child(projectile)
--- scenes/projectile.gd
+++ scenes/projectile.gd
@@ -3,7 +3,8 @@ extends Area2D
 var speed = 300.0
 var direction = Vector2.RIGHT
 var range_travelled = 0.0
-@export var damage = 1
+var damage
+var crit_chance
 
 func _physics_process(delta: float) -> void:
 	var movement = direction * speed * delta
@@ -15,7 +16,12 @@ func _physics_process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("take_damage"):
+		if _check_crit():
+			damage *= 2
 		body.take_damage(damage)
 		queue_free()
 	else:
 		queue_free()
+
+func _check_crit() -> bool:
+	return randf() < crit_chance
